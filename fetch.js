f7313c9e5680ed47863790f085baba807166e7a6
VigneshNijjani <vigneshnijjani@gmail.com>
2026-06-23 21:54:54 +0530

    code

---
+++ fetch.js
@@ -0,0 +1,11 @@
+let a=async()=>{
+    try {
+        let api=await fetch("https://fakestoreapi.com/products")
+        let data=await api.json()
+        console.log(data)
+        let p =document.getElementsByTagName(p)
+        p.t
+    } catch (error) {
+        console.log(error)
+    }
+}
